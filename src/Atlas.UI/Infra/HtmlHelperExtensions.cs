f817300ed5c673d0b6ac06a84826f555352d8432
Leonardo Cardoso <kibiluzbad@hotmail.com>
2012-09-30 12:16:16 -0300

    Refactor

---
+++ src/Atlas.UI/Infra/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atlas.UI.Infra
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString Highlight<TModel>(this HtmlHelper<TModel> helper, string value, string term)
+        {
+            if (string.IsNullOrWhiteSpace(term)) return new HtmlString(value);
+
+            value = HttpUtility.HtmlEncode(value);
+            term = HttpUtility.HtmlEncode(term);
+
+            return helper.Raw(Regex.Replace(value,term,string.Format("<span class=\"highlight\">{0}</span>", term),RegexOptions.CultureInvariant | RegexOptions.IgnoreCase ));
+        }
+    }
+}
